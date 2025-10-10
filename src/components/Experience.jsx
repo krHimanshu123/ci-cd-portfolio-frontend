b4ea8edfc90f36e904c5d9a6edbc96e5e91c75b0
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-10-10 23:16:21 +0530

    Add unordered list for experience points

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -60,6 +60,7 @@ const ExperienceCard = ({ experience }) => {
         )}
       </div>
 
+      
       <ul className='mt-5 list-disc ml-5 space-y-2'>
         {experience.points.map((point, index) => (
           <li
